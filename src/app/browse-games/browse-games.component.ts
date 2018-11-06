849bd107bc6f3e5b2980ad3b101ca296d9f9c122
Stephen Brett <S00173498@mail.itsligo.ie>
2018-11-06 15:45:22 +0000

    first change to test

--- src/app/browse-games/browse-games.component.ts
+++ src/app/browse-games/browse-games.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BrowseGamesComponent implements OnInit {
-
+  testVar:string;
   constructor() { }
 
   ngOnInit() {
