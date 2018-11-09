7d9db0a01a1bb1cf99ae7c86b668c32edd877cf4
Daire Finn <daire.finn@mail.itsligo.ie>
2018-11-09 19:24:43 +0000

    Some styling changes. Search box (Doesn't work yet)

--- src/app/browse-games/browse-games.component.ts
+++ src/app/browse-games/browse-games.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IgdbService } from '../services/igdb.service';
 import { IGame } from 'src/models/game-model';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-browse-games',
@@ -11,12 +12,18 @@ export class BrowseGamesComponent implements OnInit {
 
   games: IGame[];
   ageRatingFormat: number;
+  mobile: boolean;
+  searchTerm: FormControl = new FormControl;
 
   constructor(public gameService: IgdbService) { }
 
   ngOnInit() {
     this.ageRatingFormat = 0;
     this.games = this.gameService.getGames()
+
+    if (window.screen.width === 360) { // 768px portrait
+      this.mobile = true;
+    }
   }
 
 }
