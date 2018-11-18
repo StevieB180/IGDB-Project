e653ee9417134ca67a1659d6f5cd0b09f529a1ba
S00173510 <S00173510@VLAB-PC-72>
2018-11-18 13:19:29 +0000

    small change

--- src/app/modals/game-info/game-info.component.ts
+++ src/app/modals/game-info/game-info.component.ts
@@ -8,13 +8,22 @@ import { BrowseGamesComponent } from '../../browse-games/browse-games.component'
   templateUrl: './game-info.component.html',
   styleUrls: ['./game-info.component.scss']
 })
-export class GameInfoComponent {
+export class GameInfoComponent implements OnInit {
 
+  gameObject: IGame;
+  imageString: string;
   constructor(
     public dialogRef: MatDialogRef<GameInfoComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: IGame) { }
+    @Inject(MAT_DIALOG_DATA) public data: IGame) {
+    console.log("GIC: data.cover.url = "+data.cover.url)
+    this.gameObject = data
+     }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  ngOnInit() {
+    this.imageString = 'http:'+ this.gameObject.cover.url
   }
 }
