8ef4968892ed29d1978d366daf5453b9f12d04dc
Daire Finn <daire.finn@mail.itsligo.ie>
2018-11-29 21:18:04 +0000

    tidied up firestore service

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -23,58 +23,21 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 
 @Injectable()
 export class FirestoreService {
-reviewCollection: AngularFirestoreCollection<IReview>;
-review: Observable<IReview[]>;
-allReview: IReview[];
-errorMessage: string; 
+  reviewCollection: AngularFirestoreCollection<IReview>;
+  review: Observable<IReview[]>;
+  allReview: IReview[];
+  errorMessage: string; 
 
   constructor(private _http: HttpClient, private _afs:AngularFirestore) { 
     this.reviewCollection = _afs.collection<IReview>("review");
   }
-//add reviews to a collection
-addReview(review: IReview): void{
-   this.reviewCollection.add(review);
- }
-//get the products from the collection
-//and display them
-  // getReview(): Observable<IReview[]> {
-  //   this.review = this.reviewCollection.snapshotChanges().pipe(
-  //     map(actions => actions.map(a => {
-  //       const data = a.payload.doc.data() as IReview;
-  //       console.log("getReview:data" + JSON.stringify(data));
-  //       const id = a.payload.doc.id;
-  //       console.log("getReview:id = "+id);
-  //       return{id, ...data};
-  //     }))
-  //   );
-  //   return this.review;
- 
-  // }
-
-  // addAllReview(){
-  //   this._http.get<IReview[]>(this._productUrl).subscribe(
-  //     review => {
-  //       this.allReview = review;
-  //       for(let review of this.allReview){
-  //         console.log("Adding: " + review.gameID);
-  //         this.reviewCollection.add(review);
-  //       }
-
-  //     },
-  //     error => (this.errorMessage = <any>error)
-      
-  //   );
-  // }
+  //add reviews to a collection
+  addReview(review: IReview): void{
+    this.reviewCollection.add(review);
+  }
 
   getGameReviews(gameID: number){
     let gameReviews: Observable<IReview[]>;
-    // this._http.get<IReview[]>(this._productUrl).subscribe(r => { 
-    //     r.forEach(e => {
-    //       if(e.gameID == gameID)
-    //       { gameReviews.push(e); }
-    //     });
-    //   }
-    // )
 
     gameReviews = this.reviewCollection.snapshotChanges().pipe(
       map(action => action.map(a => {
@@ -89,9 +52,9 @@ addReview(review: IReview): void{
     return gameReviews;
   }
 
-//any errors
-private handleError(err: HttpErrorResponse){
-  console.log(err.message);
-  return Observable.throw(err.message);
-}
+  //any errors
+  private handleError(err: HttpErrorResponse){
+    console.log(err.message);
+    return Observable.throw(err.message);
+  }
 }
